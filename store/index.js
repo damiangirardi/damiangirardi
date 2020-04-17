e305fec627b2805f55569e37663add6031384562
francofuzzy <franco@thefuzzyfish.com>
2020-04-17 16:24:15 -0300

    GRAF-2 loading added before start video

---
+++ store/index.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const state = () => ({
+    videos: []
+})
+export const getters = {
+    videos:state => state.videos
+}
+export const mutations = {
+    storeVideo (state, res) {
+        state.videos = res
+    }
+}
+export const actions = {
+    getVideo  (context, url) {
+        console.log(url);
+        return axios({
+            url: `http://localhost:3000/_nuxt/${url}`,
+            method: "GET",
+            responseType: "blob"
+          }).then((response) => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            console.log(url);
+            context.commit('storeVideo', url)
+          }).catch((error) => {
+              console.log(error);
+          })
+    }
+}
+export const strict = false
