ad3d505133fa36d0c9d12eefbbc88b357e43a4cd
José Silva <jose@lunite.net>
2020-05-05 12:15:52 -0300

    Add ES-Lint + Prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,6 +81,15 @@ export default {
     */
     transpile: ['vue-instantsearch', 'instantsearch.js/es'],
     extend(config, ctx) {
-    }
-  }
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        })
+      }
+    },
+  },
 }
