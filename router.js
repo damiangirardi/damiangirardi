08859dc0be42b210a4dc593d3cbd7b8cb26a8a7c
José Silva <jose@lunite.net>
2020-05-05 12:19:27 -0300

    Apply Linter to root files

--- router.js
+++ router.js
@@ -6,30 +6,29 @@ import Home from '~/pages/index'
 import Masterplan from '~/pages/Masterplan/index'
 import Topview from '~/pages/Topview/index'
 
-
 Vue.use(Router)
 
-export function	createRouter() {
-	const router = new Router({
-		mode: 'history',
-		routes: [
-			{
-				path: '/',
-				name: 'home',
-				component: Home,
-			},
-			{
-				path: '/masterplan',
-				name: 'masterplan',
-				component: Masterplan
-			},
-			{
-				path: '/topview',
-				name: 'topview',
-				component: Topview
-			}
-		]
-	})
+export function createRouter() {
+  const router = new Router({
+    mode: 'history',
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home,
+      },
+      {
+        path: '/masterplan',
+        name: 'masterplan',
+        component: Masterplan,
+      },
+      {
+        path: '/topview',
+        name: 'topview',
+        component: Topview,
+      },
+    ],
+  })
 
-	return router
+  return router
 }
