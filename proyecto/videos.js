ba3b44560733622fb24b5373bbaa1335c068a769
Horacio de Oro <horacio@graff3d.com>
2020-05-06 15:25:18 +0000

    Ultimo sinc con GitHub

--- proyecto/videos.js
+++ proyecto/videos.js
@@ -1,50 +1,50 @@
 export const initialStep = {
-    image: 'home_green_park.jpg',
-    video: 'masterplan/Cam_01.mp4',
+    image: 'intro/01_intro.jpg',
+    video: 'masterplan/01_intro.mp4',
     blob: ''
 }
 export const guide = [
     {
         videoBefore: { 
-            path: 'Cam_02_C_rev.mp4',
+            path: '02_sec_C_rev.mp4',
             blob: ''
         },
-        image: 'posterVideos/Fot_01_home.jpg',
+        image: 'posterVideos/02_home.jpg',
         videoAfter: { 
-            path: 'Cam_02_A_2.mp4', 
+            path: '02_sec_A.mp4', 
             blob: ''
         },
         birdView: {
             videoBefore: { 
-                path: 'Cam_03_rev.mp4',
+                path: '03_sec_top.mp4',
                 blob: ''
             },
-            image: 'posterVideos/Fot_04_top.jpg',
+            image: 'posterVideos/03_sec_top.jpg',
             videoAfter: { 
-                path: 'Cam_03.mp4', 
+                path: '03_sec_top.mp4', 
                 blob: ''
             },
         }
     },
     {
         videoBefore: { 
-            path: 'Cam_02_A_1.mp4', 
+            path: '02_sec_A_rev.mp4', 
             blob: ''
         },
-        image: 'posterVideos/Fot_02_A.jpg',
+        image: 'posterVideos/02_point_A.jpg',
         videoAfter: { 
-            path: 'Cam_02_B.mp4', 
+            path: '02_sec_B.mp4', 
             blob: ''
         }
     },
     {
         videoBefore: { 
-            path: 'Cam_02_B_rev.mp4', 
+            path: '02_sec_B_rev.mp4', 
             blob: ''
         },
-        image: 'posterVideos/Fot_03_B.jpg',
+        image: 'posterVideos/02_point_B.jpg',
         videoAfter: { 
-            path: 'Cam_02_C.mp4', 
+            path: '02_sec_C.mp4', 
             blob: ''
         }
     }
